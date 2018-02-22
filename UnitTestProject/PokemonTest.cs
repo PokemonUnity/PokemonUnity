766ac9f3adc7e79f6152cb7a27b61fbe36b99fee
herbertmilhomme <herbertmilhomme@gmail.com>
2018-02-21 19:24:15 -0500

    Created Test Project, and PokemonTest Class

---
+++ UnitTestProject/PokemonTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class PokemonTest
+    {
+        #region HP/Status...
+        [TestMethod]
+        public void SetHP()
+        {
+        }
+
+        [TestMethod]
+        public void SetStatus() { }
+
+        [TestMethod]
+        public void FullyHeal() { }
+
+        [TestMethod]
+        public void MakeFainted() { }
+
+        [TestMethod]
+        public void SetPokerus() { }
+        #endregion
+
+        #region Level/stats...
+        #endregion
+
+        #region Moves...
+        #endregion
+
+        #region
+        #endregion
+    }
+}
