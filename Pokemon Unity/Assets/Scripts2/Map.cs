121148c3f728d7bb9af6bea1b81f854084d11ca3
herbertmilhomme <herbertmilhomme@gmail.com>
2018-02-24 00:01:10 -0500

    Notes for Future Me
    I was inspired by a thought, and i wanted to write down the piece of
    code before i forgot it. It's added to the project and will be changed
    and possibly used for something else later. But for now, it doesnt do
    anything, and in no way affects anything going on.

---
+++ Pokemon Unity/Assets/Scripts2/Map.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class Map : MonoBehaviour
+{
+    /// <summary>
+    /// Set this variable in the inspector
+    /// </summary>
+    public GameObject mapPrefab;
+    private Vector3[] objectPositions;
+
+    private void Awake()
+    {
+        foreach (Vector3 objectPos in objectPositions)
+        {
+            GameObject mapObject = Instantiate(mapPrefab) as GameObject;
+            mapObject.transform.position = objectPos;
+        }
+    }
+
+    static Map()
+    {
+        //objectPositions = new Array[] {} //ToDo: List of map names and vectors
+    }
+}
