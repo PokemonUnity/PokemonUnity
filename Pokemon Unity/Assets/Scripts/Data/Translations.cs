a2b1530968218996397bbc59f433d89db125dd0d
herbertmilhomme <herbertmilhomme@gmail.com>
2018-03-16 05:47:21 -0400

    WIP Translations

--- Pokemon Unity/Assets/Scripts/Data/Translations.cs
+++ Pokemon Unity/Assets/Scripts/Data/Translations.cs
@@ -1,39 +1,56 @@
 ﻿[System.Serializable]
-public class PokedexTranslation
+public class PokedexTranslation : ITranslation, ITranslationPokedex
 {
+    ITranslation _translate;
     public string Name;
+    public string PokedexEntry
+    {
+        get { return _translate.Description; }
+        set { _translate.Description = value; }
+    }
     /// <summary>
     /// "Seed", "Rat", "Plant", etc... Pokemon.
     /// </summary>
     public string Species;
-    public string PokedexEntry;
     public string[] Forms;
 }
 
 [System.Serializable]
-public class MoveTranslation
+public class MoveTranslation : ITranslation
 {
     public string Name;
     public string Description;
 }
 
 [System.Serializable]
-public class ItemTranslation
+public class ItemTranslation : ITranslation
 {
     public string Name;
     public string Description;
 }
 
 [System.Serializable]
-public class AbilityTranslation
+public class AbilityTranslation : ITranslation
 {
     public string Name;
     public string Description;
 }
 
 [System.Serializable]
-public class NatureTranslation
+public class NatureTranslation : ITranslation
 {
     public string Name;
     public string Description;
+}
+
+public interface ITranslation
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
+
+public interface ITranslationPokedex
+{
+    public string Species { get; set; }
+    public string[] Forms { get; set; }
 }
