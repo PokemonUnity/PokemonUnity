217d009d044a90b36d6d2cf220d2e9021fa77445
herbertmilhomme <herbertmilhomme@gmail.com>
2018-06-07 00:22:19 -0400

    Updating Unit Tests to match project

--- UnitTestProject/PokemonTest.cs
+++ UnitTestProject/PokemonTest.cs
@@ -80,6 +80,40 @@ namespace Tests
             pokemon.Form = 2;
             Assert.AreEqual("test1", pokemon.Name);
         }
+        [TestMethod]
+        public void Pokemon_TestPokemon_SetForm_IncreasesAtkStats()
+        {
+            Pokemon pokemon = new Pokemon(Pokemons.NONE);
+            pokemon.Form = 1;
+            Assert.AreEqual(10, pokemon.ATK);
+        }
+        #endregion
+
+        #region
+        #endregion
+    }
+    /// <summary>
+    /// Database sample on how to write code for different componenets of your database.
+    /// </summary>
+    [TestClass]
+    public class FrameworkTest
+    {
+        Pokemon.PokemonData[] Database = new Pokemon.PokemonData[] {
+            new Pokemon.PokemonData( Id: Pokemons.NONE, regionalDex: new int[1], type1: Types.NONE, type2: Types.NONE, ability1: Abilities.NONE, ability2: Abilities.NONE, hiddenAbility: Abilities.NONE,
+                        genderRatio: GenderRatio.AlwaysMale /*0f*/, catchRate: 100, eggGroup1: EggGroups.NONE, eggGroup2: EggGroups.NONE, hatchTime: 1000,
+                        height: 10f, weight: 150f, baseExpYield: 15, levelingRate: LevelingRate.ERRATIC,                        
+                        pokedexColor: Color.NONE, baseFriendship: 50,
+                        baseStatsHP: 10, //baseStatsATK: 5, baseStatsDEF: 5, baseStatsSPA: 5, baseStatsSPD: 5, baseStatsSPE: 5,
+                        luminance: 0f, movesetLevels: new int[] { 1,2,3 }, movesetMoves: new Moves[4], tmList: null, 
+                        evolution: new IPokemonEvolution[] {  new Pokemon.PokemonData.PokemonEvolution(Pokemons.ABRA, EvolutionMethod.Deaths), new Pokemon.PokemonData.PokemonEvolution<int>(Pokemons.ABRA, EvolutionMethod.Deaths, 25) },
+                        //evolutionID: null, evolutionLevel: null, evolutionMethod: null, forms: 4,
+                        heldItem: null) 
+        };
+        #region PokemonData
+        [TestMethod]
+        public void PokemonDatabase()
+        {
+        }
         #endregion
 
         #region
