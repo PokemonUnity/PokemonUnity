85d7320d8d0ba20094ca24e38d2e52444e0975cb
herbertmilhomme <herbertmilhomme@gmail.com>
2018-09-23 19:55:24 -0400

    Added Constructor Method to NPC Class

--- Pokemon Unity/Assets/Scripts2/NPC.cs
+++ Pokemon Unity/Assets/Scripts2/NPC.cs
@@ -28,7 +28,7 @@ public class NPC
 	/// Must be a number between 0 and 255.	
 	/// Optional. If undefined, the default is 30.
 	/// </summary>
-	public int 	BaseMoney { get; set; }
+	public byte BaseMoney { get; set; }
 	/// <summary>
 	/// The name of a background music (BGM) file in the folder "Audio/BGM". 
 	/// The music that plays during battles against trainers of this type. 
@@ -53,27 +53,186 @@ public class NPC
 	/// Male, Female, Mixed(i.e. if the type shows a pair of trainers)
 	/// Optional. If undefined, the default is "Mixed".
 	/// </summary>
-	public int?	Gender { get; set; }
+	public bool?	Gender { get; set; }
 	/// <summary>
 	/// The skill level of all trainers of this type, used for battle AI. 
 	/// Higher numbers represent higher skill levels. 
 	/// Must be a number between 0 and 255.	
 	/// Optional. If undefined, default is equal to the base money value.
 	/// </summary>
-	public int 	SkillLevel { get; set; }
+	public byte	SkillLevel { get; set; }
 	/// <summary>
 	/// A text field which can be used to modify the AI behaviour of all trainers of this type. 
 	/// No such modifiers are defined by default, and there is no standard format. 
 	/// See the page Battle AI for more details.
 	/// Optional. If undefined, the default is blank.
 	/// </summary>
-	public int 	SkillCodes { get; set; }
+	public int?	SkillCodes { get; set; }
 
-	public NPC()
+	public NPC(TrainerTypes trainer)
     {
+		GetTrainer(trainer);
+		//if trainer is another player
+		//Change name being loaded
     }
 
-    public static NPC[] Types
+	void GetTrainer(TrainerTypes type)
+	{
+		//Using a switch class to make sure that no one is being left out
+		#region Attempt1
+		/*foreach (NPC trainer in Types)
+		{
+			if (trainer.ID == type)
+				return trainer;
+		}*/
+		#endregion
+		#region Gender
+		#endregion
+		#region Base Money
+		switch (type)
+		{
+			case TrainerTypes.TUBER_M:
+			case TrainerTypes.TUBER2_M:
+			case TrainerTypes.TUBER_F:
+			case TrainerTypes.TUBER2_F:
+				BaseMoney = 4;
+				break;
+			case TrainerTypes.RIVAL1:
+			case TrainerTypes.BUGCATCHER:
+			case TrainerTypes.PAINTER:
+			case TrainerTypes.CAMPER:
+			case TrainerTypes.LASS:
+			case TrainerTypes.PICNICKER:
+			case TrainerTypes.SISANDBRO:
+			case TrainerTypes.SWIMMER_M:
+			case TrainerTypes.SWIMMER_F:
+			case TrainerTypes.SWIMMER2_M:
+			case TrainerTypes.SWIMMER2_F:
+			case TrainerTypes.YOUNGSTER:
+				BaseMoney = 16;
+				break;
+			case TrainerTypes.CUEBALL:
+			case TrainerTypes.CRUSHGIRL:
+			case TrainerTypes.ROCKER:
+			case TrainerTypes.TWINS:
+				BaseMoney = 24;
+				break;
+			case TrainerTypes.AROMALADY:
+			case TrainerTypes.BIKER:
+			case TrainerTypes.BIRDKEEPER:
+			case TrainerTypes.BLACKBELT:
+			case TrainerTypes.CHANELLER:
+			case TrainerTypes.FISHERMAN:
+			case TrainerTypes.HIKER:
+			case TrainerTypes.JUGGLER:
+			case TrainerTypes.PSYCHIC_M:
+			case TrainerTypes.PSYCHIC_F:
+			case TrainerTypes.SAILOR:
+			case TrainerTypes.TEAMROCKET_M:
+			case TrainerTypes.TEAMROCKET_F:
+				BaseMoney = 32;
+				break;
+			case TrainerTypes.RIVAL2:
+				BaseMoney = 36;
+				break;
+			case TrainerTypes.CRUSHKIN:
+			case TrainerTypes.TAMER:
+				BaseMoney = 48;
+				break;
+			case TrainerTypes.ENGINEER:
+			case TrainerTypes.POKEMONBREEDER:
+			case TrainerTypes.RUINMANIAC:
+			case TrainerTypes.SCIENTIST:
+			case TrainerTypes.SUPERNERD:
+				BaseMoney = 48;
+				break;
+			case TrainerTypes.BEAUTY:
+				BaseMoney = 56;
+				break;
+			case TrainerTypes.PLAYER:
+			case TrainerTypes.POKEMONTRAINER_Red:
+			case TrainerTypes.POKEMONTRAINER_Brendan:
+			case TrainerTypes.POKEMONTRAINER_Leaf:
+			case TrainerTypes.POKEMONTRAINER_May:
+			case TrainerTypes.COOLTRAINER_M:
+			case TrainerTypes.COOLTRAINER_F:
+			case TrainerTypes.POKEMONRANGER_M:
+			case TrainerTypes.POKEMONRANGER_F:
+			case TrainerTypes.YOUNGCOUPLE:
+				BaseMoney = 60;
+				break;
+			case TrainerTypes.POKEMANIAC:
+				BaseMoney = 64;
+				break;
+			case TrainerTypes.COOLCOUPLE:
+			case TrainerTypes.GAMBLER:
+			case TrainerTypes.GENTLEMAN:
+				BaseMoney = 72;
+				break;
+			case TrainerTypes.BURGLAR:
+				BaseMoney = 88;
+				break;
+			case TrainerTypes.CHAMPION:
+			case TrainerTypes.LEADER_Brock:
+			case TrainerTypes.LEADER_Surge:
+			case TrainerTypes.LEADER_Koga:
+			case TrainerTypes.LEADER_Blaine:
+			case TrainerTypes.LEADER_Giovanni:
+			case TrainerTypes.LEADER_Misty:
+			case TrainerTypes.LEADER_Erika:
+			case TrainerTypes.LEADER_Sabrina:
+			case TrainerTypes.ELITEFOUR_Lorelei:
+			case TrainerTypes.ELITEFOUR_Agatha:
+			case TrainerTypes.ELITEFOUR_Bruno:
+			case TrainerTypes.ELITEFOUR_Lance:
+			case TrainerTypes.ROCKETBOSS:
+			case TrainerTypes.PROFESSOR:
+				BaseMoney = 100;
+				break;
+			case TrainerTypes.LADY:
+				BaseMoney = 160;
+				break;
+			default:
+				BaseMoney = 30;
+				break;
+		}
+		#endregion
+		#region Skill Level
+		switch (type)
+		{
+			case TrainerTypes.BURGLAR:
+			case TrainerTypes.GAMBLER:
+				SkillLevel = 32;
+				break;
+			case TrainerTypes.LADY:
+				SkillLevel = 72;
+				break;
+			case TrainerTypes.SWIMMER_M:
+			case TrainerTypes.SWIMMER_F:
+			case TrainerTypes.SWIMMER2_M:
+			case TrainerTypes.SWIMMER2_F:
+				SkillLevel = 32;
+				break;
+			case TrainerTypes.TUBER_M:
+			case TrainerTypes.TUBER_F:
+			case TrainerTypes.TUBER2_M:
+			case TrainerTypes.TUBER2_F:
+				SkillLevel = 16;
+				break;
+			case TrainerTypes.COOLCOUPLE:
+			case TrainerTypes.SISANDBRO:
+				SkillLevel = 48;
+				break;
+			case TrainerTypes.YOUNGCOUPLE:
+				SkillLevel = 32;
+				break;
+			default:
+				break;
+		}
+		#endregion
+	}
+
+	public static NPC[] Types
 	{
 		get
 		{
