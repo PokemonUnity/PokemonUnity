c433aaf7514e4cc6f5e7d881849c999082b76870
herbertmilhomme <herbertmilhomme@gmail.com>
2018-09-30 02:52:05 -0400

    Added Unity ItemHandler

---
+++ Pokemon Unity/Assets/Scripts2/EventHandlers/ItemHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class ItemHandler : UnityEngine.MonoBehaviour
+{
+    void Awake()
+    {
+    }
+
+    void OnEnable1()
+    {
+    }
+    void Start()
+    {        
+    }
+
+    void Update()
+    {
+    }	
+}
