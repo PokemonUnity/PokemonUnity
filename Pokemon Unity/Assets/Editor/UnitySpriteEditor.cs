c24a2ee1b69eb5c04fbc0cf37477c2a9e0b1f201
herbertmilhomme <herbertmilhomme@gmail.com>
2018-11-02 07:38:26 -0400

    Modified For Front/Back Frames
    Front and Back Sprites have different frame counts

--- Pokemon Unity/Assets/Editor/UnitySpriteEditor.cs
+++ Pokemon Unity/Assets/Editor/UnitySpriteEditor.cs
@@ -80,14 +80,14 @@ public class EditorHelper : MonoBehaviour
         bool Add2List = false;*/
 
         //Texture2D[] textures = new Texture2D[totalSprites];
-        Texture2D[] textures = Resources.LoadAll<Texture2D>("Sprites/Pokemon/FRONT/[R]").OrderBy(t => t.name.PadNumbers()).ToArray();
+        Texture2D[] textures = Resources.LoadAll<Texture2D>("Sprites/Pokemon/BACK/[R]").OrderBy(t => t.name.PadNumbers()).ToArray();
         //Texture2D[] textures = AssetDatabase.IsValidFolder.Resources.LoadAll<Texture2D>("Sprites/Pokemon/FRONT/[R]");
         //Texture2D[] front_regular = Resources.LoadAll<Texture2D>("Sprites/Pokemon/FRONT/[R]");
         //Texture2D[] front_shiny = Resources.LoadAll<Texture2D>("Sprites/Pokemon/FRONT/[S]");
         //Texture2D[] back_regular = Resources.LoadAll<Texture2D>("Sprites/Pokemon/BACK/[R]");
         //Texture2D[] back_shiny = Resources.LoadAll<Texture2D>("Sprites/Pokemon/BACK/[S]");
 
-        int[] frames = new int[] {
+        int[] Front_frames = new int[] {
             //gen1
             99,111,167,167,107,89,143,51,87,244,81,111,89,89,123,79,60,41,35,71,101,101,119,119,61,21,71,74,112,112,167,167,123,111,89,49,123,121,99,97,133,102,71,53,111,109,54,54,16,16,109,95,95,71,71,73,71,95,21,108,83,139,107,44,76,120,56,141,71,79,107,89,113,119,119,105,105,149,98,59,95,97,71,95,80,103,47,91,91,47,89,113,105,143,65,189,189,155,155,49,121,116,103,87,101,110,25,88,99,153,125,125,73,97,71,49,53,89,157,136,112,81,70,87,99,157,153,193,193,116,63,253,80,64,114,114,96,96,79,159,127,137,137,107,95,65,143,79,120,120,87,87,65,80,125,101,99,119,60,68,155,67,47,17,173,51,17,53,49,127,102,125,119,
             //gen2
@@ -100,31 +100,44 @@ public class EditorHelper : MonoBehaviour
             98,145,96,147,117,125,165,295,101,193,105,167,127,117,151,65,117,174,155,95,95,74,101,181,60,85,149,150,150,159,153,129,117,197,160,110,103,167,121,133,143,139,184,97,192,111,127,156,127,99,179,138,233,127,117,107,120,185,185,104,118,104,158,133,144,144,81,119,143,167,117,144,144,127,183,127,80,203,135,118,179,141,101,139,159,191,121,159,143,83,125,107,145,214,131,131,131,131,139,139,139,139,188,175,238,99,95,165,165,137,137,117,63,143,163,131,107,171,171,210,85,126,113,105,155,131,167,150,106,182,185,224,143,123,218,123,133,135,127,81,160,134,157,153,142,41,139,141,96,72,126,113,75,117,48,135,161,183,307,235,188,197,223,191,317,191,172,357,113,105,144,152,299,179,179,179,179,179
         };
 
+        int[] Back_frames = new int[] {
+            //gen1
+            99,111,167,167,107,89,143,51,87,244,81,111,29,29,123,79,20,41,35,68,99,99,119,119,61,21,71,73,113,113,167,167,123,111,82,49,121,121,99,105,132,102,71,107,109,109,54,54,16,16,109,95,95,71,71,74,65,95,21,108,83,139,107,46,81,120,51,135,71,79,107,89,111,121,121,105,105,149,95,59,95,97,71,95,80,106,47,91,91,48,89,113,105,143,65,189,189,155,155,49,121,113,103,87,101,110,25,88,143,153,123,123,73,97,70,45,53,81,157,133,108,81,72,87,99,167,167,193,193,116,63,253,80,64,114,114,96,96,79,159,127,137,137,107,95,65,143,81,117,117,87,87,65,80,133,107,99,119,53,68,155,67,47,17,173,51,17,53,49,127,102,125,119,
+            //gen2
+			51,119,89,89,128,82,112,87,175,83,71,110,131,72,97,97,40,40,65,33,95,63,126,113,64,161,101,101,129,97,97,99,103,73,107,135,145,67,67,95,95,79,95,95,78,78,65,90,42,97,97,69,69,143,65,61,61,101,95,95,90,95,95,95,95,95,95,95,95,95,95,95,95,93,95,95,95,95,95,95,95,95,95,95,95,95,95,120,120,63,63,95,54,95,67,67,95,95,104,125,65,105,105,103,84,84,113,113,83,124,124,64,63,55,66,66,113,91,33,32,118,167,112,102,96,96,54,107,127,127,95,85,95,66,129,95,95,67,95,96,175,111,95,86,63,85,120,83,64,
+            //gen3
+            108,148,135,149,149,125,125,113,113,139,116,93,85,113,100,113,61,90,154,154,93,162,162,115,85,83,83,259,89,89,107,107,65,137,151,142,131,101,241,114,171,54,51,40,90,70,107,74,17,58,115,115,114,132,92,14,147,150,92,155,31,110,117,287,287,89,89,83,77,144,144,55,45,98,98,93,93,99,99,127,125,107,111,49,49,111,111,137,131,29,114,37,85,125,102,114,114,23,101,71,92,167,223,143,191,149,99,81,101,141,133,103,153,91,199,199,83,84,102,76,86,86,131,86,116,85,108,137,107,63,99,85,111,92,84,113,108,113,113,92,53,78,133,69,175,103,103,83,83,107,76,243,101,116,144,158,105,169,99,
+            //gen4
+            77,113,109,140,127,133,149,125,131,98,98,117,117,92,92,103,103,167,167,149,149,101,101,127,127,192,192,171,171,108,203,203,93,175,116,122,163,163,163,225,225,225,18,32,32,224,112,112,119,119,168,168,117,79,95,103,103,121,109,191,191,127,71,183,105,29,85,124,120,163,95,95,125,109,155,159,65,160,223,125,125,55,55,89,89,143,125,143,191,191,95,95,105,177,155,155,127,127,115,173,173,171,171,96,111,111,81,81,127,127,95,131,251,251,191,191,103,160,95,112,137,171,131,145,145,95,134,237,95,131,171,171,160,171,213,192,191,145,263,137,127,203,145,26,157,191,143,119,143,173,109,158,158,158,158,158,158,158,158,158,158,158,158,158,158,158,158,165,
+            //gen5
+            109,146,99,139,115,126,165,295,106,221,103,169,127,117,151,65,123,174,161,95,95,77,101,181,60,84,149,160,154,157,153,129,117,197,160,110,103,169,118,133,143,139,190,121,192,111,127,159,127,99,179,139,233,113,117,107,116,178,181,104,120,123,157,101,144,144,81,119,143,165,117,144,144,127,183,127,80,203,135,118,179,141,101,139,159,191,123,159,143,83,125,107,145,214,131,131,131,131,139,139,139,139,186,175,238,99,95,165,165,137,137,117,63,143,171,131,107,171,171,210,85,126,113,104,155,131,167,150,106,185,189,224,143,124,218,125,129,135,127,81,159,134,159,153,142,41,140,141,96,74,117,120,75,117,48,136,162,179,307,235,189,197,223,191,305,191,173,357,113,100,145,152,299,179,179,179,179,179
+		};
+
         object[] args = new object[2] { 0, 0 };
         System.Reflection.MethodInfo mi = typeof(TextureImporter).GetMethod("GetWidthAndHeight", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
 
-        for (int z = 475; z < textures.Length; z++)//
+        for (int z = 630; z < textures.Length; z++)//
         {
             // path is Assets/Resources/Sprites/Sprite (1).png
             // path is Assets/Resources/Sprites/Pokemon/PokemonIcons/unfixed double size/icon013.png
             //textures[z] = Resources.Load<Texture2D>("Sprites/Sprite (" + (z + 1) + ")");
             string path = System.IO.Path.GetFileName(AssetDatabase.GetAssetPath(textures[z]));
             //Debug.Log(path + "; " + AssetDatabase.GetAssetPath(textures[z]));
-            TextureImporter ti_FR = AssetImporter.GetAtPath("Assets/Resources/Sprites/Pokemon/FRONT/[R]/" + path) as TextureImporter;
+            //TextureImporter ti_FR = AssetImporter.GetAtPath("Assets/Resources/Sprites/Pokemon/FRONT/[R]/" + path) as TextureImporter;
             //TextureImporter ti_BR = AssetDatabase.IsValidFolder("Sprites/Pokemon/BACK/[R]") ? AssetImporter.GetAtPath("Sprites/Pokemon/BACK/[R]/"+path) as TextureImporter:null;
             TextureImporter ti_BR = AssetImporter.GetAtPath("Assets/Resources/Sprites/Pokemon/BACK/[R]/" + path) as TextureImporter;
             //Shiny
-            TextureImporter ti_FS = AssetImporter.GetAtPath("Assets/Resources/Sprites/Pokemon/FRONT/[S]/" + path) as TextureImporter;
+            //TextureImporter ti_FS = AssetImporter.GetAtPath("Assets/Resources/Sprites/Pokemon/FRONT/[S]/" + path) as TextureImporter;
             TextureImporter ti_BS = AssetImporter.GetAtPath("Assets/Resources/Sprites/Pokemon/BACK/[S]/" + path) as TextureImporter;
 
             //TextureImporterSettings tis = new TextureImporterSettings();
             //ti.SetTextureSettings(tis);
-
-            if (ti_FR.textureType != TextureImporterType.Sprite) ti_FR.textureType = TextureImporterType.Sprite;
+			/*
+            //if (ti_FR.textureType != TextureImporterType.Sprite) ti_FR.textureType = TextureImporterType.Sprite;
             if (ti_BR.textureType != TextureImporterType.Sprite) ti_BR.textureType = TextureImporterType.Sprite;
             //Shiny
-            if (ti_FS.textureType != TextureImporterType.Sprite) ti_FS.textureType = TextureImporterType.Sprite;
-            if (ti_BS.textureType != TextureImporterType.Sprite) ti_BS.textureType = TextureImporterType.Sprite;
+            //if (ti_FS.textureType != TextureImporterType.Sprite) ti_FS.textureType = TextureImporterType.Sprite;
+            //if (ti_BS.textureType != TextureImporterType.Sprite) ti_BS.textureType = TextureImporterType.Sprite;
 
             //Debug.Log("Max Texture Size: " + ti.maxTextureSize);
             //Debug.Log("Compression Quality: " + ti.compressionQuality);
@@ -132,33 +145,33 @@ public class EditorHelper : MonoBehaviour
 
             //new TextureImporterPlatformSettings().
             //TextureResizeAlgorithm.Bilinear;
-            ti_FR.isReadable = ti_BR.isReadable = ti_FS.isReadable = ti_BS.isReadable = true;
-            if (ti_FR.spriteImportMode == SpriteImportMode.Multiple) {
-                // Bug? Need to convert to single then back to multiple in order to make changes when it's already sliced
-                ti_FR.spriteImportMode = SpriteImportMode.Single;
-                AssetDatabase.ImportAsset("Assets/Resources/Sprites/Pokemon/FRONT/[R]/" + path, ImportAssetOptions.ForceUpdate);
-            }
+            ti_BR.isReadable = true;// = ti_FR.isReadable = ti_FS.isReadable = ti_BS.isReadable
+            //if (ti_FR.spriteImportMode == SpriteImportMode.Multiple) {
+            //    // Bug? Need to convert to single then back to multiple in order to make changes when it's already sliced
+            //    ti_FR.spriteImportMode = SpriteImportMode.Single;
+            //    AssetDatabase.ImportAsset("Assets/Resources/Sprites/Pokemon/FRONT/[R]/" + path, ImportAssetOptions.ForceUpdate);
+            //}
             if (ti_BR.spriteImportMode == SpriteImportMode.Multiple) {
                 // Bug? Need to convert to single then back to multiple in order to make changes when it's already sliced
                 ti_BR.spriteImportMode = SpriteImportMode.Single;
                 AssetDatabase.ImportAsset("Assets/Resources/Sprites/Pokemon/BACK/[R]/" + path, ImportAssetOptions.ForceUpdate);
             }
-            if (ti_FS.spriteImportMode == SpriteImportMode.Multiple) {
-                // Bug? Need to convert to single then back to multiple in order to make changes when it's already sliced
-                ti_FS.spriteImportMode = SpriteImportMode.Single;
-                AssetDatabase.ImportAsset("Assets/Resources/Sprites/Pokemon/FRONT/[S]/" + path, ImportAssetOptions.ForceUpdate);
-            }
-            if (ti_BS.spriteImportMode == SpriteImportMode.Multiple) {
-                // Bug? Need to convert to single then back to multiple in order to make changes when it's already sliced
-                ti_BS.spriteImportMode = SpriteImportMode.Single;
-                AssetDatabase.ImportAsset("Assets/Resources/Sprites/Pokemon/BACK/[S]/" + path, ImportAssetOptions.ForceUpdate);
-            }
-            ti_FR.spriteImportMode = ti_BR.spriteImportMode = ti_FS.spriteImportMode = ti_BS.spriteImportMode = SpriteImportMode.Multiple;
-
+            //if (ti_FS.spriteImportMode == SpriteImportMode.Multiple) {
+            //    // Bug? Need to convert to single then back to multiple in order to make changes when it's already sliced
+            //    ti_FS.spriteImportMode = SpriteImportMode.Single;
+            //    AssetDatabase.ImportAsset("Assets/Resources/Sprites/Pokemon/FRONT/[S]/" + path, ImportAssetOptions.ForceUpdate);
+            //}
+            //if (ti_BS.spriteImportMode == SpriteImportMode.Multiple) {
+            //    // Bug? Need to convert to single then back to multiple in order to make changes when it's already sliced
+            //    ti_BS.spriteImportMode = SpriteImportMode.Single;
+            //    AssetDatabase.ImportAsset("Assets/Resources/Sprites/Pokemon/BACK/[S]/" + path, ImportAssetOptions.ForceUpdate);
+            //}
+            ti_BR.spriteImportMode = SpriteImportMode.Multiple;// = ti_FR.spriteImportMode = ti_FS.spriteImportMode = ti_BS.spriteImportMode
+			*/
  
             List<SpriteMetaData> newData = new List<SpriteMetaData>();
 
-            mi.Invoke(ti_FR, args);//textures[z]
+            mi.Invoke(ti_BR, args);//textures[z]
             
             /*AnimationClip newClip = new AnimationClip();
             newClip.wrapMode = WrapMode.Loop;
@@ -209,7 +222,7 @@ public class EditorHelper : MonoBehaviour
             {
                 for (int x = 0; x < Width; x += SliceWidth)
                 {
-                    if (n == frames[z]) break;// continue;
+                    if (n == Back_frames[z]) break;// continue;
                     SpriteMetaData smd = new SpriteMetaData();
                     smd.pivot = new Vector2(0.5f, 0f);
                     smd.alignment = 9;
@@ -241,22 +254,22 @@ public class EditorHelper : MonoBehaviour
                 }
             }
             //Debug.Assert(newData.Count == frames[z]);
-            if(newData.Count == frames[z])
-            Debug.Log(string.Format("{0}; Frame Count: {1}; Frame Slices: {2}",newData.Count == frames[z], newData.Count, frames[z]));
-            else Debug.LogWarning(string.Format("{0}; Frame Count: {1}; Frame Slices: {2}; Image Name: {3}; #: {4}",newData.Count == frames[z], newData.Count, frames[z], textures[z].name, z+1));
+            if(newData.Count == Back_frames[z])
+            Debug.Log(string.Format("{0}; Frame Count: {1}; Frame Slices: {2}",newData.Count == Back_frames[z], newData.Count, Back_frames[z]));
+            else Debug.LogWarning(string.Format("{0}; Frame Count: {1}; Frame Slices: {2}; Image Name: {3}; #: {4}",newData.Count == Back_frames[z], newData.Count, Back_frames[z], textures[z].name, z+1));
 
             /*AnimationUtility.SetAnimationClipSettings(newClip, acs);
             AnimationUtility.SetObjectReferenceCurve(newClip, spriteBinding, spriteKeyframes);
             AssetDatabase.CreateAsset(newClip, "assets/anims/pokemon/" + pokeNum + ".anim");*/
 
-            ti_FR.spritesheet = ti_BR.spritesheet = ti_FS.spritesheet = ti_BS.spritesheet = newData.ToArray();
-            AssetDatabase.ImportAsset("Assets/Resources/Sprites/Pokemon/FRONT/[R]/" + path, ImportAssetOptions.ForceUpdate);
+            ti_BR.spritesheet = ti_BS.spritesheet = newData.ToArray();// = ti_FR.spritesheet = ti_FS.spritesheet
+            //AssetDatabase.ImportAsset("Assets/Resources/Sprites/Pokemon/FRONT/[R]/" + path, ImportAssetOptions.ForceUpdate);
             AssetDatabase.ImportAsset("Assets/Resources/Sprites/Pokemon/BACK/[R]/" + path, ImportAssetOptions.ForceUpdate);
-            AssetDatabase.ImportAsset("Assets/Resources/Sprites/Pokemon/FRONT/[S]/" + path, ImportAssetOptions.ForceUpdate);
-            AssetDatabase.ImportAsset("Assets/Resources/Sprites/Pokemon/BACK/[S]/" + path, ImportAssetOptions.ForceUpdate);
-            ti_FR.filterMode = FilterMode.Point;// ti_FR.textureCompression = TextureImporterCompression.Uncompressed; ti_FR.maxTextureSize = 2048; // ti_FR.compressionQuality = 0;
+            //AssetDatabase.ImportAsset("Assets/Resources/Sprites/Pokemon/FRONT/[S]/" + path, ImportAssetOptions.ForceUpdate);
+            //AssetDatabase.ImportAsset("Assets/Resources/Sprites/Pokemon/BACK/[S]/" + path, ImportAssetOptions.ForceUpdate);
+            //ti_FR.filterMode = FilterMode.Point;// ti_FR.textureCompression = TextureImporterCompression.Uncompressed; ti_FR.maxTextureSize = 2048; // ti_FR.compressionQuality = 0;
             ti_BR.filterMode = FilterMode.Point;// ti_BR.textureCompression = TextureImporterCompression.Uncompressed; ti_BR.maxTextureSize = 2048; // ti_BR.compressionQuality = 0;
-            ti_FS.filterMode = FilterMode.Point;// ti_FS.textureCompression = TextureImporterCompression.Uncompressed; ti_FS.maxTextureSize = 2048; // ti_FS.compressionQuality = 0;
+            //ti_FS.filterMode = FilterMode.Point;// ti_FS.textureCompression = TextureImporterCompression.Uncompressed; ti_FS.maxTextureSize = 2048; // ti_FS.compressionQuality = 0;
             ti_BS.filterMode = FilterMode.Point;// ti_BS.textureCompression = TextureImporterCompression.Uncompressed; ti_BS.maxTextureSize = 2048; // ti_BS.compressionQuality = 0;
         }
         //Will only reimport the first icon... will need to reimport the rest if the first is good enough.
