ae2513d99b745222119f83a9824896d05cbb0e6e
herbertmilhomme <herbertmilhomme@gmail.com>
2018-11-05 05:49:31 -0500

    Attempted a Unity Battle Scene Handler WIP

--- Pokemon Unity/Assets/Scripts2/EventHandlers/BattleAnimationHandler.cs
+++ Pokemon Unity/Assets/Scripts2/EventHandlers/BattleAnimationHandler.cs
@@ -2,45 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using UnityEngine;
 
 class BattleAnimationHandler : UnityEngine.MonoBehaviour
 {
+	//private static GameVariables PersistantPlayerData { get { return StartupSceneHandler.PersistantPlayerData; } }
+	public static IEnumerator<Battle.BattleResults> BattleCoroutineResults { get; private set; }
+	public IEnumerator<Battle.BattleResults> BattleCoroutine { get; private set; }
     public float time;
     AnimationClip anim;
 
-    void Awake()
+	void Awake()
     {
         //anim = this.GetComponent<Animator>()
         //anim = this.GetComponent<Animator>()
-    }
-    void OnEnable1()
-    {
-    }
-    void Start()
-    {
-        
     }
 
+    void OnEnable()
+    {
+
+    }
+
+    void Start()
+    {
+	}
 
     void Update()
     {
-        /* Ping GameNetwork server every 15-45secs
+		/* Ping GameNetwork server every 15-45secs
          * If game server is offline or unable to
          * ping connection:
          * Netplay toggle-icon bool equals false
          * otherwise toggle bool to true
          */
-        //StartCoroutine(PingServerEveryXsec);
-        //Use coroutine that has a while loop instead of using here
-        /*while (MainMenu.activeSelf)
+		//StartCoroutine(PingServerEveryXsec);
+		//Use coroutine that has a while loop instead of using here
+		/*while (MainMenu.activeSelf)
         {
             //While scene is enabled, run coroutine to ping server
             break;
         }*/
-        //int index = (int)(UnityEngine.Time.timeSinceLevelLoad * Settings.framesPerSecond);
-        //index = index % sprites[].Length;
-    }
+		//int index = (int)(UnityEngine.Time.timeSinceLevelLoad * Settings.framesPerSecond);
+		//index = index % sprites[].Length;string[] dir = AssetDatabase.GetSubFolders("Assets/Pokemons");
+	}
 
 
     /* Create an animation controller
--- Pokemon Unity/Assets/Scripts2/EventHandlers/StartupSceneHandler.cs
+++ Pokemon Unity/Assets/Scripts2/EventHandlers/StartupSceneHandler.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine.EventSystems;
+//using UnityEngine;
 
-class StartupSceneHandler : UnityEngine.MonoBehaviour, UnityEngine.EventSystems.ISubmitHandler, UnityEngine.EventSystems.IScrollHandler
+public class StartupSceneHandler : UnityEngine.MonoBehaviour, UnityEngine.EventSystems.ISubmitHandler, UnityEngine.EventSystems.IScrollHandler
 {
-	private static GameVariables PersistantPlayerData;
+	#region Variables
+	public static GameVariables PersistantPlayerData { get; private set; }
     private static UnityEngine.GameObject MainMenu;// = UnityEngine.GameObject.Find("MainMenu");
     /// <summary>
     /// This is the panel display that shows save data for currently selected CONTINUE option
@@ -15,6 +17,11 @@ class StartupSceneHandler : UnityEngine.MonoBehaviour, UnityEngine.EventSystems.
     private static UnityEngine.GameObject MenuOptions;// = MainMenu.transform.GetChild(1).gameObject;
     //private static UnityEngine.UI.Text DialogUITextDump = MainMenu.GetComponent<UnityEngine.UI.Text>();
     //private static UnityEngine.UI.Text DialogUIScrollText = UnityEngine.GameObject.Find("DialogScrollText").GetComponent<UnityEngine.UI.Text>();
+	//map class with headers
+	//pokemon array for list of encounters on current map loaded
+	#endregion
+
+	#region Unity MonoBehavior
     void Awake()
     {
 		PersistantPlayerData = new GameVariables();
@@ -22,6 +29,7 @@ class StartupSceneHandler : UnityEngine.MonoBehaviour, UnityEngine.EventSystems.
         MainMenu = UnityEngine.GameObject.Find("MainMenu");
         FileDataPanel = MainMenu.transform.GetChild(0).gameObject;
         MenuOptions = MainMenu.transform.GetChild(1).gameObject;
+		//ToDo: Awake Audio Components
     }
     void OnEnable1()
     {
@@ -53,13 +61,10 @@ class StartupSceneHandler : UnityEngine.MonoBehaviour, UnityEngine.EventSystems.
             //MenuOptions.transform.GetChild(2).gameObject.GetComponent<UnityEngine.RectTransform>().ForceUpdateRectTransforms();
             //MenuOptions.transform.GetChild(3).gameObject.GetComponent<UnityEngine.RectTransform>().ForceUpdateRectTransforms();
         }
-
     }
     void Start()
     {
     }
-
-
     void Update()
     {
         /* Ping GameNetwork server every 15-45secs
@@ -78,14 +83,15 @@ class StartupSceneHandler : UnityEngine.MonoBehaviour, UnityEngine.EventSystems.
         //int index = (int)(UnityEngine.Time.timeSinceLevelLoad * Settings.framesPerSecond);
         //index = index % sprites[].Length;
     }
+	#endregion
 
-
-    /* If Continue option is available:
+	#region GameStart and Main Menu
+	/* If Continue option is available:
      * file slot data should reflect in the 
      * playerData window on the right side;
      * disable slot options with no data
      */
-    void ContinueSavedGame()
+	void ContinueSavedGame()
     {
         //If Continue Option is select
         if (MenuOptions.transform.GetChild(0).gameObject.GetComponent<UnityEngine.UI.Toggle>().isOn)
@@ -121,7 +127,84 @@ class StartupSceneHandler : UnityEngine.MonoBehaviour, UnityEngine.EventSystems.
         }
     }
 
-    /* If settings option is accessed, 
+	/* If settings option is accessed, 
      * Use GameVariables.ChangeScene to transition
      */
+	#endregion
+		
+	#region Methods
+	//Your map class should be the one talking to unity and triggering if battles should occur
+
+	/// <summary>
+	/// Start a single wild battle
+	/// </summary>
+	public bool WildBattle(Pokemon pkmn, bool cantescape = true, bool canlose = false)
+	{
+		if (GameVariables.playerTrainer.Trainer.Party.Length == 0 || (UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.LeftControl) && GameVariables.debugMode))
+		{
+			if (GameVariables.playerTrainer.Trainer.Party.Length > 0)
+				GameVariables.DebugLog("SKIPPING BATTLE...");
+			GameVariables.nextBattleBGM = null;
+			GameVariables.nextBattleME = null;
+			GameVariables.nextBattleBack = null;
+			return true;
+		}
+		Pokemon[] generateWildPkmn = new Pokemon[1];
+		generateWildPkmn[0] = pkmn; //new Pokemon();
+		//int decision = 0;
+		Battle battle =
+		//GameVariables.battle =
+			new Battle(
+				GameVariables.playerTrainer.Trainer,
+				new Trainer(generateWildPkmn)
+			)
+			.InternalBattle(true)
+			.CantEscape(!cantescape);
+			//.StartBattle(canlose); //Switch to battle scene and trigger coroutine 
+			//.AfterBattle(ref decision,canlose);
+		//GameVariables.battle.StartBattle(canlose);  
+		IEnumerator<Battle.BattleResults> e = BattleAnimationHandler.BattleCoroutineResults;
+		//while battle scene is active
+		//delay results of battle
+		//on battle end return the results of the battle 
+		//ToDo: and save data to profile?... maybe that would be done from battle class
+		return e.Current != Battle.BattleResults.LOST;
+	}
+
+	/// <summary>
+	/// Start a double wild battle
+	/// </summary>
+	public bool DoubleWildBattle(Pokemon pkmn1, Pokemon pkmn2, bool cantescape = true, bool canlose = false)
+	{
+		if (GameVariables.playerTrainer.Trainer.Party.Length == 0 || (UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.LeftControl) && GameVariables.debugMode))
+		{
+			if (GameVariables.playerTrainer.Trainer.Party.Length > 0)
+				GameVariables.DebugLog("SKIPPING BATTLE...");
+			GameVariables.nextBattleBGM = null;
+			GameVariables.nextBattleME = null;
+			GameVariables.nextBattleBack = null;
+			return true;
+		}
+		Pokemon[] generateWildPkmn = new Pokemon[] { pkmn1, pkmn2 };//new Pokemon(), new Pokemon()
+		//int decision = 0;
+		Battle battle =
+		//GameVariables.battle =
+			new Battle(
+				GameVariables.playerTrainer.Trainer,
+				new Trainer(generateWildPkmn) { IsDouble = true }
+			)
+			.InternalBattle(true)
+			.CantEscape(!cantescape);
+			//.StartBattle(canlose); //Switch to battle scene and trigger coroutine 
+			//.AfterBattle(ref decision,canlose);
+		//GameVariables.battle.StartBattle(canlose);  
+		IEnumerator<Battle.BattleResults> e = BattleAnimationHandler.BattleCoroutineResults;
+		//while battle scene is active
+		//delay results of battle
+		//on battle end return the results of the battle 
+		//ToDo: and save data to profile?... maybe that would be done from battle class
+		//return battle.decision;
+		return (e.Current != Battle.BattleResults.LOST && e.Current != Battle.BattleResults.DRAW);
+	}
+	#endregion
 }
