213cc409e9bb2aaf82d07aad276b8d473cb58a98
Carlos Brown <hocdrive@gmail.com>
2019-01-08 17:30:52 -0800

    Superior version, reverting changes

--- TOOLS/Tiled2PkUnity/tiled2pkunity.js
+++ TOOLS/Tiled2PkUnity/tiled2pkunity.js
@@ -1,68 +1,52 @@
+//Tiled2PkUnity Version 2.0: "I should have redone this last month" Edition
 //Do not edit or save this JS file using Notepad or any other non-code text editor.
 //Please use a formatted editor such as Visual Studio Code (NOT Visual Studio) or Notepad++ in order to modify this file.
-
-//Tiled2PkUnity was developed in Node.js by TeamPopplio. 
+//This app was developed in Node.js by TeamPopplio.
 //Please ask me for help/support with this app rather than contacting the Pokemon Unity team.
-console.log(process.argv.join(" "))
-if(!process.argv.slice(2)[0] || !process.argv.slice(2)[1]) 
-    console.log("Missing arguments!")
-    //node tiled2pkunity.js ./map.json [./collision.txt]
-    //node tiled2pkunity.js (Map JSON) [(Collision File)]
-    //Only the map's JSON is required
-    
-var fs = require('fs'), //Filesystem Module
+if(!process.argv.slice(2)[0]) {
+    console.log("You are missing the tilemap's JSON file in the arguments.\n")
+    process.exit()
+}
+var fs, //Filesystem module, if installed
     json = require(process.argv.slice(2)[0]), //JSON file
     filename = process.argv.slice(2)[2], //Filename
-    tiles = json.layers[0].data, //JSON Tilemap
-    tileset = {file:json.tilesets[0],c:[],nc:[],w:[],e2:[]}, //Tileset data
-    compiled = []; //Compiled map data
-if(!filename)
-    filename = process.argv.slice(2)[0].split(".json").push("_COMP.txt").join("")
-
-//Tileset Terrain Tags\\
-/*console.log("\nCreating tileset terrain tags...\n")
-for(var z = 0; z< tileset.file.tilecount; z++)
-{
-    var tile = tileset.file.tileproperties[z.toString()]
-    if(tile.Collision)
-    {
-        tileset.c.push(z+1)
-        console.log("Tileset #" + z.toString() + " = Collidable")
-    }
-    else if(tile.Water)
-    {
-        tileset.w.push(z+1)
-        console.log("Tileset #" + z.toString() + " = Water")
-    }
-    else if(tile.Environment2)
-    {
-        tileset.nc.push(z+1)
-        console.log("Tileset #" + z.toString() + " = Env2")
-    }
-    else
-    {
-        nocollision.push(z+1)
-        console.log("Tileset #" + z.toString() + " = Non-Collidable")
-    }
-}*/
-    
-//Collision\\
-console.log("\nLayer 1:\n")
-for(var l = 0; a< json.layers.length; a++) {
-    var layer = compiled.push([])
-    for(var a = 0; a< json.layers[l].data.length; a++) {
-        if((json.layers[l].data[a]-1) < 0)
-        {
-            layer.push(0) //Blank tile, no collision
-            console.log("Tile #"+json.layers[l].data[a])
-        }
-        else
-            layer.push(tileset.file.tileproperties[(json.layers[l].data[a]-1).toString()]["Type"])
-    }
+    compiled = [], //Compiled map data
+    p1 = true; //For the phases in the compilation
+if(!filename) filename = process.argv.slice(2)[0].replace(".json","_cmap.txt") //If you did not provide a filename
+try{fs = require('fs')}
+catch(e) {
+    if (e.code == 'MODULE_NOT_FOUND') {
+        console.log("You do not have the FS module installed.\nCollision data will instead be outputted via the console.\n")
+        fs = false;
+    } else throw e;
 }
-//Writing the file\\
-fs.writeFileSync(filename,collision.join(" "), (err) => {
-    if (err) throw err;
-    console.log("\nFinished conversion process for " + process.argv.slice(2)[0] + ", printed collision map to " + filename + "\n")
-})
-//Version 1.5
+//Compiling\\
+console.log("Compiling collsion map for "+process.argv.slice(2)[0]+":")
+for(var l = 0; l< json.layers.length; l++) {
+    console.log("\nLayer "+(l+1)+":\n")
+    for(var a = 0; a< json.layers[l].data.length; a++) {
+        if((json.layers[l].data[a]-1) < 0) {
+            if(p1) {
+                compiled.push(0) //Blank tile, no collision
+                console.log("Tile #"+(a+1)+" = Floor/Blank")
+            }
+        } else {
+            try{var t = json.tilesets[0].tileproperties[(json.layers[l].data[a]-1).toString()]["Type"]}
+            catch(e) {console.log("Cannot find the type of tile #"+(a+1)+"!");t=NaN}
+            if(!isNaN(t)) {
+                if(p1) compiled.push(t)
+                else compiled[a] = t //Replace whatever was in this same position
+                if(t == 2) console.log("Tile #"+(a+1)+" = Water")
+                else if(t == 3) console.log("Tile #"+(a+1)+" = Environment2")
+                else if(!t) console.log("Tile #"+(a+1)+" = Floor")
+                else console.log("Tile #"+(a+1)+" = Wall")
+            }
+        }
+    }
+    p1 = false //Next layers should instead merge.
+}
+if(fs) {
+    fs.writeFileSync(filename,compiled.join(" "), (err) => {if (err) throw err;})
+    console.log("\nFinished compilation process for " + process.argv.slice(2)[0] + ", printed collision map to: \n" + filename + "\n")
+}
+else console.log("\nFinished compilation process for " + process.argv.slice(2)[0] + ":\n\n"+compiled.join(" ") + "\n")
