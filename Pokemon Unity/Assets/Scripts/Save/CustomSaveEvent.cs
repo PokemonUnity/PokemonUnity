5bbc6a497c82f3b734672b237e6a610e5eeb0e31
Velorexe <rawrexe313@gmail.com>
2019-01-13 22:38:29 +0100

    Due to Submodule: added Save feature

---
+++ Pokemon Unity/Assets/Scripts/Save/CustomSaveEvent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+
+[System.Serializable]
+public class CustomSaveEvent
+{
+    public DateTime EventTime;
+    public SaveEventType EventType;
+    public int SceneIndex;
+    public string ObjectName;
+    public SerializableVector3 ObjectPosition;
+
+    public CustomSaveEvent(DateTime eventTime, SaveEventType eventType, GameObject eventObject, int sceneIndex)
+    {
+        EventTime = eventTime;
+        EventType = eventType;
+        SceneIndex = sceneIndex;
+
+        ObjectName = eventObject.name;
+        ObjectPosition = eventObject.transform.position;
+    }
+
+    public override string ToString()
+    {
+        return ("EventType : " + EventType.ToString() + ", Time : " + EventTime + ", Map: " + SceneIndex);
+    }
+}
---
+++ Pokemon Unity/Assets/Scripts/Save/EventListener.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class EventListener : MonoBehaviour
+{
+    public GameObject Player;
+    private PlayerMovement playerMovement;
+    private bool RegisteredEvent = false;
+
+    private void Awake()
+    {
+        GlobalSaveManager.Load(0);
+
+        if (null == Player)
+        {
+            Player = GameObject.FindGameObjectWithTag("Player");
+            playerMovement = Player.GetComponent<PlayerMovement>();
+        }
+        GlobalSaveManager.RegisterPlayer(Player);
+
+        List<CustomSaveEvent> Events = GlobalSaveManager.GetRelaventSaveData(SceneManager.GetActiveScene().buildIndex);
+
+        foreach(CustomSaveEvent Event in Events)
+        {
+            var objects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == Event.ObjectName);
+            foreach (GameObject saveObject in objects)
+            {
+                if (saveObject.name == Event.ObjectName && saveObject.transform.position == Event.ObjectPosition)
+                {
+                    switch (Event.EventType)
+                    {
+                        case SaveEventType.ITEM:
+                            saveObject.SetActive(false);
+                            break;
+                        case SaveEventType.INTERACTION:
+                            saveObject.SetActive(false);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (playerMovement.busyWith != null && RegisteredEvent == false)
+        {
+            //Getting the Object the Player's busy with
+            GameObject eventObject = playerMovement.busyWith;
+
+            //Getting the events from the object
+            CustomEvent interactionEvent = eventObject.GetComponent<CustomEvent>();
+            InteractItem itemEvent = eventObject.GetComponent<InteractItem>();
+
+            if (null != interactionEvent)
+            {
+                RegisteredEvent = true;
+                GlobalSaveManager.RegisterEvent(new CustomSaveEvent(DateTime.Now, SaveEventType.INTERACTION, eventObject, SceneManager.GetActiveScene().buildIndex));
+            }
+            else if (null != itemEvent)
+            {
+                RegisteredEvent = true;
+                GlobalSaveManager.RegisterEvent(new CustomSaveEvent(DateTime.Now, SaveEventType.ITEM, eventObject, SceneManager.GetActiveScene().buildIndex));
+                GlobalSaveManager.Save();
+            }
+        }
+        else if(playerMovement.busyWith == null)
+        {
+            RegisteredEvent = false;
+        }
+    }
+}
