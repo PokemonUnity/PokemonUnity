75e126f2b72b83e2787fc64177b0c6646fd27734
herbertmilhomme <herbertmilhomme@gmail.com>
2019-03-27 16:57:35 -0400

    Create MathHelper.cs

---
+++ Pokemon Unity/Assets/Scripts2/Extensions/MathHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public static class MathHelper
+{
+    //private MathHelper()
+    //{
+    //    throw new InvalidOperationException("Cannot initialize static class.");
+    //}
+
+	public static int Clamp(float amount, float min, float max)
+	{
+		if (amount > max)
+			return (int)max;
+		else if (amount < min)
+			return (int)min;
+		else
+			return (int)amount;
+	}
+
+	public static int Clamp(this float amount, int min, int max)
+	{
+		if (amount > max)
+			return max;
+		else if (amount < min)
+			return min;
+		else
+			return (int)amount;
+	}
+
+	public static int Clamp(this int amount, int min, int max)
+	{
+		if (amount > max)
+			return max;
+		else if (amount < min)
+			return min;
+		else
+			return amount;
+	}
+}
