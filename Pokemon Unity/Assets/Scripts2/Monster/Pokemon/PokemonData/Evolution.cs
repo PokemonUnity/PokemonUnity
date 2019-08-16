0609ac97e7dcfc7b9ec3c94a8a40bb3e9fe40e22
Herbert Milhomme <herb@herbertmilhomme.com>
2019-08-15 21:07:59 -0400

    Update Evolution.cs
    Repurposed the commented code into a new method to ensure proper usage of class.

--- Pokemon Unity/Assets/Scripts2/Monster/Pokemon/PokemonData/Evolution.cs
+++ Pokemon Unity/Assets/Scripts2/Monster/Pokemon/PokemonData/Evolution.cs
@@ -64,101 +64,8 @@ namespace PokemonUnity.Monster
                 /// </summary>
                 public T EvolveValue { get; private set; }
 
-                //public PokemonEvolution<T> (T objects){}
-                //void evolve(PokemonData.Pokemon EvolveTo, EvolutionMethod EvolveHow, T Value) { }
-
                 public PokemonEvolution(Pokemons EvolveTo, EvolutionMethod EvolveHow, T Value) : base(EvolveTo: EvolveTo, EvolveHow: EvolveHow)
-                {
-					/* ToDo: Repurpose this inside method that checks pokemon evolve values
-					//dynamic value = null;
-                    #region Switch
-                    //This should trigger after the class has been initialized, right?
-                    switch (EvolveHow)
-                    {
-                        case EvolutionMethod.Level:
-                        case EvolutionMethod.LevelFemale:
-                        case EvolutionMethod.LevelMale:
-                        case EvolutionMethod.Ninjask:
-                        case EvolutionMethod.Beauty:
-                        case EvolutionMethod.Happiness:
-                        case EvolutionMethod.HappinessDay:
-                        case EvolutionMethod.HappinessNight:
-                        case EvolutionMethod.Hatred:
-                            //if(typeof(T) || this.GetType() == typeof(string))
-                            if (!typeof(T).Equals(typeof(int)))
-							{
-                                //throw new Exception("Type not acceptable for Method-Value pair.");
-                                //Instead of throwing an exception, i'll correct the problem instead?
-								//int.TryParse(Value.ToString, out EvolveValue);
-								Convert.ChangeType(EvolveValue, typeof(int));
-								Convert.ChangeType(Value, typeof(int));
-								//Value = 0; //default(T);
-								//value = 0;
-								this.EvolveValue = 0;
-							}
-                            break;
-                        case EvolutionMethod.Item:
-                        case EvolutionMethod.ItemFemale:
-                        case EvolutionMethod.ItemMale:
-                        case EvolutionMethod.TradeItem:
-                        case EvolutionMethod.HoldItem:
-                        case EvolutionMethod.HoldItemDay:
-                        case EvolutionMethod.HoldItemNight:
-                            if (!typeof(T).Equals(typeof(Items)))
-							{
-								Convert.ChangeType(EvolveValue, typeof(Items));
-								Convert.ChangeType(Value, typeof(Items));
-								//Value = Items.NONE; //default(T);
-								//value = Items.NONE; //default(T);
-								this.EvolveValue = Items.NONE;
-							}
-                            break;
-                        case EvolutionMethod.TradeSpecies:
-                        case EvolutionMethod.Party:
-                        //case EvolutionMethod.Shedinja:
-                            if (!typeof(T).Equals(typeof(Pokemons)))
-							{
-								Convert.ChangeType(EvolveValue, typeof(Pokemons));
-								Convert.ChangeType(Value, typeof(Pokemons));
-								//Value = Pokemons.NONE; //default(T);
-								//value = Pokemons.NONE; //default(T);
-								this.EvolveValue = Pokemons.NONE;
-							}
-                            break;
-                        case EvolutionMethod.Move:
-                            if (!typeof(T).Equals(typeof(Moves)))
-							{
-								Convert.ChangeType(EvolveValue, typeof(Moves));
-								Convert.ChangeType(Value, typeof(Moves));
-								//Value = Moves.NONE; //default(T);
-								//value = Moves.NONE; //default(T);
-								this.EvolveValue = Moves.NONE;
-							}
-                            break;
-                        case EvolutionMethod.Type:
-                            if (!typeof(T).Equals(typeof(Types)))
-							{
-								Convert.ChangeType(EvolveValue, typeof(Types));
-								Convert.ChangeType(Value, typeof(Types));
-								//Value = Types.NONE; //default(T);
-								//value = Types.NONE; //default(T);
-								this.EvolveValue = Types.NONE;
-							}
-                            break;
-                        case EvolutionMethod.Shedinja:
-                        case EvolutionMethod.Time:
-                        case EvolutionMethod.Season:
-                        case EvolutionMethod.Location:
-                        case EvolutionMethod.Weather:
-                        default:
-							//if there's no problem, just ignore it, and move on...
-							//value = Value;
-							this.EvolveValue = Value;
-							break;
-                    }
-					#endregion
-					//this.EvolveValue = value; //Value;
-					//this.EvolveValue = Convert.ChangeType(value, typeof(T));*/
+                {					
                 }
                 /*public PokemonEvolution(PokemonData.Pokemon EvolveTo, EvolutionMethod EvolveHow, T Value) : base(EvolveTo: EvolveTo, EvolveHow: EvolveHow) {
                     #region Switch
@@ -235,7 +142,113 @@ namespace PokemonUnity.Monster
                 {
                     return true;// base.isGenericT();
                 }
-            }
+
+				/// <summary>
+				/// This is used to ensure that the constructor class was created properly,
+				/// by returning a new instance if the class was used incorrectly.
+				/// </summary>
+				/// <returns>returns a valid <see cref="PokemonEvolution{T}"/>.this</returns>
+				public IPokemonEvolution Validate()
+				{
+					//dynamic value = null;
+                    #region Switch
+                    //This should trigger after the class has been initialized, right?
+                    switch (EvolveHow)
+                    {
+                        case EvolutionMethod.Level:
+                        case EvolutionMethod.LevelFemale:
+                        case EvolutionMethod.LevelMale:
+                        case EvolutionMethod.Ninjask:
+                        case EvolutionMethod.Beauty:
+                        case EvolutionMethod.Happiness:
+                        case EvolutionMethod.HappinessDay:
+                        case EvolutionMethod.HappinessNight:
+                        case EvolutionMethod.Hatred:
+                            //if(typeof(T) || this.GetType() == typeof(string))
+                            if (!typeof(T).Equals(typeof(int)))
+							{
+								//throw new Exception("Type not acceptable for Method-Value pair.");
+								//Instead of throwing an exception, i'll correct the problem instead?
+								//int.TryParse(Value.ToString, out EvolveValue);
+								//Convert.ChangeType(EvolveValue, typeof(int));
+								//Convert.ChangeType(Value, typeof(int));
+								//Value = 0; //default(T);
+								//value = 0;
+								//this.EvolveValue = 0;
+								return new PokemonEvolution<int>(this.Species, this.EvolveMethod, 0);
+							}
+                            break;
+                        case EvolutionMethod.Item:
+                        case EvolutionMethod.ItemFemale:
+                        case EvolutionMethod.ItemMale:
+                        case EvolutionMethod.TradeItem:
+                        case EvolutionMethod.HoldItem:
+                        case EvolutionMethod.HoldItemDay:
+                        case EvolutionMethod.HoldItemNight:
+                            if (!typeof(T).Equals(typeof(Items)))
+							{
+								//Convert.ChangeType(EvolveValue, typeof(Items));
+								//Convert.ChangeType(Value, typeof(Items));
+								//Value = Items.NONE; //default(T);
+								//value = Items.NONE; //default(T);
+								//this.EvolveValue = Items.NONE;
+								return new PokemonEvolution<Items>(this.Species, this.EvolveMethod, Items.NONE);
+							}
+                            break;
+                        case EvolutionMethod.TradeSpecies:
+                        case EvolutionMethod.Party:
+                        //case EvolutionMethod.Shedinja:
+                            if (!typeof(T).Equals(typeof(Pokemons)))
+							{
+								//Convert.ChangeType(EvolveValue, typeof(Pokemons));
+								//Convert.ChangeType(Value, typeof(Pokemons));
+								//Value = Pokemons.NONE; //default(T);
+								//value = Pokemons.NONE; //default(T);
+								//this.EvolveValue = Pokemons.NONE;
+								return new PokemonEvolution<Pokemons>(this.Species, this.EvolveMethod, Pokemons.NONE);
+							}
+                            break;
+                        case EvolutionMethod.Move:
+                            if (!typeof(T).Equals(typeof(Moves)))
+							{
+								//Convert.ChangeType(EvolveValue, typeof(Moves));
+								//Convert.ChangeType(Value, typeof(Moves));
+								//Value = Moves.NONE; //default(T);
+								//value = Moves.NONE; //default(T);
+								//this.EvolveValue = Moves.NONE;
+								return new PokemonEvolution<int>(this.Species, this.EvolveMethod, Moves.NONE);
+							}
+                            break;
+                        case EvolutionMethod.Type:
+                            if (!typeof(T).Equals(typeof(Types)))
+							{
+								//Convert.ChangeType(EvolveValue, typeof(Types));
+								//Convert.ChangeType(Value, typeof(Types));
+								//Value = Types.NONE; //default(T);
+								//value = Types.NONE; //default(T);
+								//this.EvolveValue = Types.NONE;
+								return new PokemonEvolution<int>(this.Species, this.EvolveMethod, Types.NONE);
+							}
+                            break;
+                        case EvolutionMethod.Shedinja:
+                        case EvolutionMethod.Time:
+                        case EvolutionMethod.Season:
+                        case EvolutionMethod.Location:
+                        case EvolutionMethod.Weather:
+                        default:
+							//if there's no problem, just ignore it, and move on...
+							//value = Value;
+							//this.EvolveValue = Value;
+							break;
+                    }
+					#endregion
+					//this.EvolveValue = value; //Value;
+					//this.EvolveValue = Convert.ChangeType(value, typeof(T));
+					return this;
+				}
+
+				//void evolve(PokemonData.Pokemon EvolveTo, EvolutionMethod EvolveHow, T Value) { }
+			}
         }
     }
 }
