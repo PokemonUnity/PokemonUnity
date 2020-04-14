c70b5e13bb065e6d369baa43990bc80e3bc3b1f8
Herbert Milhomme <herb@herbertmilhomme.com>
2020-04-14 15:01:03 -0400

    Added IsNotNullOrNone Bool Extension

--- PokemonUnity.Shared/Extensions/MoveExtension.cs
+++ PokemonUnity.Shared/Extensions/MoveExtension.cs
@@ -3,7 +3,15 @@
 namespace PokemonUnity.Attack
 {
 	public static class MoveExtension
-	{		
+	{
+		public static bool IsNotNullOrNone(this PokemonUnity.Attack.Move move)
+		{
+			return move != null || move.MoveId != Moves.NONE;
+		}
+		public static bool IsNotNullOrNone(this PokemonUnity.Battle.Move move)
+		{
+			return move != null || move.MoveId != Moves.NONE;
+		}
 		public static string ToString(this PokemonUnity.Moves move, TextScripts text)
 		{
 			//create a switch, and return Locale Name or Description
--- PokemonUnity.Shared/Extensions/PokemonExtension.cs
+++ PokemonUnity.Shared/Extensions/PokemonExtension.cs
@@ -4,6 +4,14 @@ namespace PokemonUnity.Monster
 {
 	public static class PokemonExtension
 	{
+		public static bool IsNotNullOrNone(this PokemonUnity.Monster.Pokemon pokemon)
+		{
+			return pokemon != null || pokemon.Species != Pokemons.NONE; 
+		}
+		public static bool IsNotNullOrNone(this PokemonUnity.Battle.Pokemon pokemon)
+		{
+			return pokemon != null || pokemon.Species != Pokemons.NONE; 
+		}
 		public static string ToString(this PokemonUnity.Pokemons pokemon, TextScripts text)
 		{
 			//create a switch, and return Locale Name or Description
