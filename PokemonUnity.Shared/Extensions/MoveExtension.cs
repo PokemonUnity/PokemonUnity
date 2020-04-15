c80c5e461706c525b58af5b599d68ab8ddcd8976
Herbert Milhomme <herb@herbertmilhomme.com>
2020-04-15 04:34:03 -0400

    Update MoveExtension.cs

--- PokemonUnity.Shared/Extensions/MoveExtension.cs
+++ PokemonUnity.Shared/Extensions/MoveExtension.cs
@@ -4,6 +4,25 @@ namespace PokemonUnity
 {
 	public static class MoveExtension
 	{
+		#region Targets
+		public static bool HasMultipleTargets(this PokemonUnity.Attack.Data.Targets target)
+		{
+			return
+				//target == Attack.Data.Targets.ENTIRE_FIELD ||
+				//target == Attack.Data.Targets.OPPONENTS_FIELD ||
+				//target == Attack.Data.Targets.USERS_FIELD ||
+				target == Attack.Data.Targets.ALL_OPPONENTS ||
+				target == Attack.Data.Targets.ALL_OTHER_POKEMON ||
+				target == Attack.Data.Targets.ALL_POKEMON; 
+		}
+		public static bool TargetsOneOpponent(this PokemonUnity.Attack.Data.Targets target)
+		{
+			return
+				target == Attack.Data.Targets.SELECTED_POKEMON ||
+				target == Attack.Data.Targets.SELECTED_POKEMON_ME_FIRST ||
+				target == Attack.Data.Targets.RANDOM_OPPONENT; ; 
+		}
+		#endregion
 		public static bool IsNotNullOrNone(this PokemonUnity.Attack.Move move)
 		{
 			return move != null || move.MoveId != Moves.NONE;
