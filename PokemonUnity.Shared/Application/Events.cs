403759e496165128934e57002c3e82ad35853081
Herbert Milhomme <herb@herbertmilhomme.com>
2020-11-24 23:29:50 -0500

    Update Events.cs

--- PokemonUnity.Shared/Application/Events.cs
+++ PokemonUnity.Shared/Application/Events.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using PokemonUnity.Monster;
+using PokemonUnity.Utility;
 
 namespace PokemonUnity
 {
@@ -17,6 +18,11 @@ namespace PokemonUnity
 	public static partial class Events
 	{
 		#region EventHandlers
+		//public event EventHandler<OnLevelUpEventArgs> OnLevelUp;
+		//public event EventHandler<OnBattlePhaseEventArgs> OnBattlePhase;
+		//public event EventHandler<OnHPChangedEventArgs> OnHPChanged;
+		//public event EventHandler<OnFaintedEventArgs> OnFainted;
+		//public event EventHandler<OnDisplayEventArgs> OnDisplay;
 		/// <summary>
 		/// Fires whenever the player moves to a new map. Event handler receives the old
 		/// map ID or 0 if none.  Also fires when the first map of the game is loaded
@@ -272,6 +278,120 @@ namespace PokemonUnity
 			/// Map associated with the spriteset (not necessarily the current map).
 			/// </summary>
 			public int MapId { get; set; }
+		}
+		#endregion
+
+		#region Event Sender / Raise Events
+		public static void OnMapCreateTrigger()
+		{
+			//OnMapCreateEventArgs e = new OnMapCreateEventArgs();
+			if (OnMapCreate != null) OnMapCreate.Invoke(null, new OnMapCreateEventArgs());
+		}
+		public static void OnMapChangeTrigger()
+		{
+			OnMapChangeEventArgs e = new OnMapChangeEventArgs(); 
+			if(OnMapChange != null) OnMapChange.Invoke(null, e);
+		}
+		public static void OnMapChangingTrigger()
+		{
+			OnMapChangingEventArgs e = new OnMapChangingEventArgs(); 
+			if(OnMapChanging != null) OnMapChanging.Invoke(null, e);
+		}
+		/// <summary>
+		/// Parameters:
+		/// e[0] - Event that just left the tile.
+		/// e[1] - Map ID where the tile is located (not necessarily
+		///  the current map). Use "Game.GameData.MapFactory.getMap(e[1])" to
+		///  get the Game_Map object corresponding to that map.
+		/// e[2] - X-coordinate of the tile
+		/// e[3] - Y-coordinate of the tile
+		/// </summary>
+		public static void OnLeaveTileTrigger(object @event, int mapId, float x, float y, float z) { OnLeaveTileTrigger(@event, mapId, new Vector(x, y, z)); }
+		public static void OnLeaveTileTrigger(object @event, int mapId, Vector tile)
+		{
+			OnLeaveTileEventArgs e = new OnLeaveTileEventArgs();
+			//EventHandler<OnLeaveTileEventArgs> handler = OnLeaveTile;
+			//if (handler != null) handler.Invoke(null, e);
+			if (OnLeaveTile != null) OnLeaveTile.Invoke(null, e);
+		}
+		/// <summary>
+		/// Parameters:
+		/// e[0] - Event that just entered a tile.
+		/// </summary>
+		public static void OnStepTakenFieldMovementTrigger()
+		{
+			OnStepTakenFieldMovementEventArgs e = new OnStepTakenFieldMovementEventArgs(); 
+			if(OnStepTakenFieldMovement != null) OnStepTakenFieldMovement.Invoke(null, e);
+		}
+		/// <summary>
+		/// Parameters:
+		/// e[0] = Array that contains a single boolean value.
+		/// If an event handler moves the player to a new map, it should set this value
+		/// to true. Other event handlers should check this parameter's value.
+		/// </summary>
+		public static void OnStepTakenTransferPossibleTrigger()
+		{
+			OnStepTakenTransferPossibleEventArgs e = new OnStepTakenTransferPossibleEventArgs(); 
+			if(OnStepTakenTransferPossible != null) OnStepTakenTransferPossible.Invoke(null, e);
+		}
+		//public static void OnWildBattleOverrideTrigger(null,Pokemons species,int level,bool handled)
+		//{
+		//	OnWildBattleOverrideEventArgs e = new OnWildBattleOverrideEventArgs();
+		//	if(OnWildBattleOverride != null) OnWildBattleOverride.Invoke(null, e);
+		//}
+		/// <summary>
+		/// Parameters: 
+		/// e[0] - Pokémon species
+		/// e[1] - Pokémon level
+		/// e[2] - Battle result (1-win, 2-loss, 3-escaped, 4-caught, 5-draw)
+		/// </summary>
+		public static void OnWildBattleEndTrigger()
+		{
+			OnWildBattleEndEventArgs e = new OnWildBattleEndEventArgs(); 
+			if(OnWildBattleEnd != null) OnWildBattleEnd.Invoke(null, e);
+		}
+		/// <summary>
+		/// Parameters: 
+		/// e[0] - Pokémon being created
+		/// </summary>
+		public static void OnWildPokemonCreateTrigger()
+		{
+			OnWildPokemonCreateEventArgs e = new OnWildPokemonCreateEventArgs(); 
+			if(OnWildPokemonCreate != null) OnWildPokemonCreate.Invoke(null, e);
+		}
+		/// <summary>
+		/// Parameters: 
+		/// e[0] - Trainer
+		/// e[1] - Items possessed by the trainer
+		/// e[2] - Party
+		/// </summary>
+		public static void OnTrainerPartyLoadTrigger()
+		{
+			OnTrainerPartyLoadEventArgs e = new OnTrainerPartyLoadEventArgs(); 
+			if(OnTrainerPartyLoad != null) OnTrainerPartyLoad.Invoke(null, e);
+		}
+		/// <summary>
+		/// Parameters:
+		/// e[0] = Scene_Map object.
+		/// e[1] = Whether the player just moved to a new map (either true or false). If
+		///   false, some other code had called <see cref="Game.GameData.Scene.createSpritesets"/>
+		///   to regenerate the map scene without transferring the player elsewhere
+		/// </summary>
+		public static void OnMapSceneChangeTrigger()
+		{
+			OnMapSceneChangeEventArgs e= new OnMapSceneChangeEventArgs(); 
+			if(OnMapSceneChange != null) OnMapSceneChange.Invoke(null, e);
+		}
+		/// <summary>
+		/// Parameters:
+		/// e[0] = Spriteset being created
+		/// e[1] = Viewport used for tilemap and characters
+		/// e[0].map = Map associated with the spriteset (not necessarily the current map).
+		/// </summary>
+		public static void OnSpritesetCreateTrigger()
+		{
+			OnSpritesetCreateEventArgs e = new OnSpritesetCreateEventArgs(); 
+			if(OnSpritesetCreate != null) OnSpritesetCreate.Invoke(null, e);
 		}
 		#endregion
 	}
