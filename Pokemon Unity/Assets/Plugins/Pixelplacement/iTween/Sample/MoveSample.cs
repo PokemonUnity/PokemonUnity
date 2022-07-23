1842b55b803ec696847a08adae02f72294325246
Herb M <herbertmilhomme@gmail.com>
2022-07-23 17:51:29 -0400

    Removed Unity Project from Repository

--- Pokemon Unity/Assets/Plugins/Pixelplacement/iTween/Sample/MoveSample.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class MoveSample : MonoBehaviour
-{	
-	void Start(){
-		//iTween.MoveBy(gameObject, iTween.Hash("x", 2, "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .1));
-	}
-}
